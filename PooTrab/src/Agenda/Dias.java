48260a6355625d628310fb4c433afba8ef84abf8
Rafael <Rafael@DESKTOP-J533FJR>
2017-12-06 23:28:17 -0200

    classe dias

---
+++ PooTrab/src/Agenda/Dias.java
@@ -0,0 +1,72 @@
+package Agenda;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Dias {
+
+    public String pegarDiaSem() {
+        Date d = new Date();
+        Calendar c = new GregorianCalendar();
+        c.setTime(d);
+        String nome = "";
+        int dia = c.get(c.DAY_OF_WEEK);
+        switch (dia) {
+            case Calendar.SUNDAY:
+                nome = "Domingo";
+                break;
+            case Calendar.MONDAY:
+                nome = "Segunda";
+                break;
+            case Calendar.TUESDAY:
+                nome = "Terça";
+                break;
+            case Calendar.WEDNESDAY:
+                nome = "Quarta";
+                break;
+            case Calendar.THURSDAY:
+                nome = "Quinta";
+                break;
+            case Calendar.FRIDAY:
+                nome = "Sexta";
+                break;
+            case Calendar.SATURDAY:
+                nome = "sábado";
+                break;
+        }
+        return nome;
+    }
+
+    public int pegarDiaMes() {
+        Date d = new Date();
+        Calendar c = new GregorianCalendar();
+        c.setTime(d);
+        String nome = "";
+        int dia = c.get(c.DAY_OF_WEEK);
+        switch (dia) {
+            case Calendar.SUNDAY:
+                nome = "Domingo";
+                break;
+            case Calendar.MONDAY:
+                nome = "Segunda";
+                break;
+            case Calendar.TUESDAY:
+                nome = "Terça";
+                break;
+            case Calendar.WEDNESDAY:
+                nome = "Quarta";
+                break;
+            case Calendar.THURSDAY:
+                nome = "Quinta";
+                break;
+            case Calendar.FRIDAY:
+                nome = "Sexta";
+                break;
+            case Calendar.SATURDAY:
+                nome = "sábado";
+                break;
+        }
+        return dia;
+    }
+}
