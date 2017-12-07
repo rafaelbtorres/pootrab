c4200c271258f3c3622f7927bee86750b31ac17e
Rafael <Rafael@DESKTOP-J533FJR>
2017-12-06 23:30:16 -0200

    cxontrole

--- PooTrab/src/Agenda/Dias.java
+++ PooTrab/src/Agenda/Dias.java
@@ -43,30 +43,7 @@ public class Dias {
         Calendar c = new GregorianCalendar();
         c.setTime(d);
         String nome = "";
-        int dia = c.get(c.DAY_OF_WEEK);
-        switch (dia) {
-            case Calendar.SUNDAY:
-                nome = "Domingo";
-                break;
-            case Calendar.MONDAY:
-                nome = "Segunda";
-                break;
-            case Calendar.TUESDAY:
-                nome = "Terça";
-                break;
-            case Calendar.WEDNESDAY:
-                nome = "Quarta";
-                break;
-            case Calendar.THURSDAY:
-                nome = "Quinta";
-                break;
-            case Calendar.FRIDAY:
-                nome = "Sexta";
-                break;
-            case Calendar.SATURDAY:
-                nome = "sábado";
-                break;
-        }
+        int dia = c.get(c.DAY_OF_WEEK);       
         return dia;
     }
 }
